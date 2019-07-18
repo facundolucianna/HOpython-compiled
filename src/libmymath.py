5958d0fcfa1e1ba4337ec607e9119cabf972a742
Facundo Adrian Lucianna <facundolucianna@gmail.com>
2019-07-17 23:38:52 -0300

    Agregada las funciones de arrays.c

--- src/libmymath.py
+++ src/libmymath.py
@@ -1,4 +1,5 @@
 import ctypes as C
+import numpy as np
 
 #Creamos la clase con todas las funciones de C
 math = C.CDLL('./libmymath.so')
@@ -62,3 +63,40 @@ state = math.add_float_ref(C.byref(a), C.byref(b), C.byref(res))
 output = res.value
 
 print("Resultado = " + str(output) + " y salida = " + str(state))
+
+#int add_int_array(int *a, int *b, int *c, int n)
+intp = C.POINTER(C.c_int)
+print("Funcion add_int_array()")
+
+math.add_int_array.restype = C.c_int
+
+arr1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+arr2 = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+num = len(arr1)
+
+in1 = np.array(arr1, dtype=C.c_int)
+in2 = np.array(arr2, dtype=C.c_int)
+out = np.zeros(10, dtype=C.c_int)
+n = C.c_int(num)
+
+math.add_int_array(in1.ctypes.data_as(intp), in2.ctypes.data_as(intp), out.ctypes.data_as(intp), n)
+
+print("El array de salida es " + str(out))
+
+# float dot_product(float *a, float *b, int n)
+flp = C.POINTER(C.c_float)
+print("Funcion dot_product()")
+
+math.dot_product.restype = C.c_float
+
+arr1 = [1, 1, 2]
+arr2 = [2, 1, 2]
+num = len(arr1)
+
+in1 = np.array(arr1, dtype=C.c_float)
+in2 = np.array(arr2, dtype=C.c_float)
+n = C.c_int(num)
+
+out = math.dot_product(in1.ctypes.data_as(flp), in2.ctypes.data_as(flp), n)
+
+print("El producto vectorial es " + str(out))
